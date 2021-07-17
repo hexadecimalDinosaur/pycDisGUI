0f0d051d9cd4646849d8c91081fbca54bd74ae0d
UserBlackBox <userblackbox@tutanota.com>
2021-07-17 17:38:38 -0400

    create sub objects for code objects within bytecode

--- xdis_parse.py
+++ xdis_parse.py
@@ -1,26 +1,34 @@
 import os
-import subprocess
 import xdis.load
 import xdis.std as dis
+from xdis import iscode
 
-class xdisBytecode:
-    filename = None
-    code = None
-    constants = None
-    name = None
+
+class XdisBytecode:
+    filename = ""
+    code = []
+    constants = ()
+    name = ""
+    sub = []
 
     @classmethod
     def from_file(self, path:str):
         if not os.path.exists(path):
             raise FileNotFoundError
         (version, timestamp, magic_int, co, is_pypy, source_size, sip_hash) = xdis.load.load_module(path)
-        return xdisBytecode(co, co.co_filename)
+        return XdisBytecode(co, co.co_filename, True)
 
-    def __init__(self, co:code, filename:str):
+    def __init__(self, co:code, filename:str, file_as_name:bool):
         self.filename = filename
         self.code = list(dis.get_instructions(co))
         self.name = co.co_name
+        if file_as_name:
+            self.name = filename
         self.constants = co.co_consts
+
+        for const in co.co_consts:
+            if iscode(const):
+                self.sub.append(XdisBytecode(const, filename, False))
 
     def get_bytecode(self):
         code = ""
